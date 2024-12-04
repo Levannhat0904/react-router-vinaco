718c400781b58a55bbffc56aeca75fd78f1de53e
LEVANNHAT <hiamnhatdz203@gmail.com>
2024-12-04 14:37:28 +0700

    navigate

--- src/page/error404/index.tsx
+++ src/page/error404/index.tsx
@@ -1,6 +1,15 @@
-function Error404(){
-        return(
-                <h1>404 page</h1>
-        )
+import { useEffect } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
+
+function Error404() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    setTimeout(() => {
+      navigate("/", {
+        state: "redirect from location",
+      });
+    }, 2000);
+  }, [navigate]);
+  return <h1>404 page</h1>;
 }
 export default Error404;
--- src/page/home/index.tsx
+++ src/page/home/index.tsx
@@ -1,4 +1,8 @@
+import { useLocation } from "react-router-dom"
+
 function Home(){
+         const location = useLocation()
+         console.log(location)
         return(
                 <h1>Home</h1>
         )
